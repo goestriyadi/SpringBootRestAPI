f5adeefa372bba460d5ad8d216426d5cfdd87747
Goes <goesmodul@gmail.com>
2023-09-12 19:36:38 +0700

    First Commit - Hello World Rest-API

---
+++ src/main/java/com/goesmodul/springboot/firstrestapi/helloworld/HelloWorldResource.java
@@ -0,0 +1,21 @@
+package com.goesmodul.springboot.firstrestapi.helloworld;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller
+@RestController //Return the value instead of view
+public class HelloWorldResource {
+	//hello-world = "Hello World"
+	@RequestMapping("/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	@RequestMapping("/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+}
